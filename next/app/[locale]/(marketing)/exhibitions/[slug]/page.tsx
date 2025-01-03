24c3caf11797dfb9a8b6a037647be0279097af91
Ivan Balic <balicivan2@gmail.com>
2025-01-04 00:15:05 +0100

    Exhibitions | Add empty page

---
+++ next/app/[locale]/(marketing)/exhibitions/[slug]/page.tsx
@@ -0,0 +1,9 @@
+import { Container } from "@/components/container";
+
+export default function ExhibitionDetails() {
+    return (
+        <Container className="text-black pt-[75px] md:pt-[100px] lg:pt-[125px]">
+            Hello World!
+        </Container>
+    );
+}
