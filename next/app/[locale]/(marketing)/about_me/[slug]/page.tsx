fa8d30ff940f6f44cef8d3eba5c21f7da161c456
Ivan Balic <balicivan2@gmail.com>
2025-04-26 00:58:36 +0200

    Use original size signed award sections and sync font sizes

--- next/app/[locale]/(marketing)/about_me/[slug]/page.tsx
+++ next/app/[locale]/(marketing)/about_me/[slug]/page.tsx
@@ -30,15 +30,18 @@ export default function AboutMeSubPage({ params: { locale, slug} }: { params: { 
             )}
         >
             <SubNavBredCrumbs navItems={Object.values(MENU_ITEMS_CONFIG)} locale={locale} page={slug} subItemClass="max-[521px]:flex-1 max-[521px]:truncate" />
-            <Link href={`/${locale}/about_me`} className="flex items-center cursor-pointer mt-2 w-6 md:w-8 lg:w-10 max-w-[40px]">
-                <Image src={arrowLeft} alt="arrowLeft" className="border-primaryBlue" />
-            </Link>
-            <div className="my-6 sm:my-7 md:my-8 lg:my-9 xl:my-10 font-bold text-[22px] sm:text-[26px] md:text-[30px] lg:text-[34px] xl:text-[36px] leading-[25px] sm:leading-[29px] md:leading-[33px] lg:leading-[37px] xl:leading-[39px] tracking-[.15em]">
-                <span className="text-primaryBlue">{getTitle(slug, locale)}</span>
+            <div className="flex gap-8 items-center my-6 sm:my-7 md:my-8 lg:my-9 xl:my-10 font-bold text-[22px] sm:text-[26px] md:text-[30px] lg:text-[34px] xl:text-[36px] leading-[25px] sm:leading-[29px] md:leading-[33px] lg:leading-[37px] xl:leading-[39px] tracking-[.15em]">
+                <Link href={`/${locale}/about_me`} className="flex items-center cursor-pointer w-6 md:w-8 lg:w-10 max-w-[40px]">
+                    <Image src={arrowLeft} alt="arrowLeft" className="border-primaryBlue" />
+                </Link>
+                <span className="text-primaryBlue font-roboto-serif">{getTitle(slug, locale)}</span>
             </div>
             <div className="flex flex-col gap-0 sm:gap-0.5 md:gap-1 lg:gap-1.5 xl:gap-2 mb-20 text-[16px] sm:text-[18px] md:text-[20px] lg:text-[22px] xl:text-[24px] leading-[31px] sm:leading-[33px] md:leading-[35px] lg:leading-[37px] xl:leading-[39px] font-helvetica tracking-[.15em]">
                 {getContent(slug, locale).map((item, index) => (
-                    <span key={index}>{item}</span>
+                    <div className="flex gap-1" key={index}>
+                        <span className="text-primaryBlue">{item.YEAR}</span>
+                        <span>{item.AWARD}</span>
+                    </div>
                 ))}
             </div>
         </Container>
--- next/app/[locale]/(marketing)/about_me/page.tsx
+++ next/app/[locale]/(marketing)/about_me/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 
 import { ITranslations } from "@/types/types";
 import { Container } from "@/components/container";
-import aboutMeCover from '@/public/images/about-me/about-me-cover.png';
+import aboutMeCover from '@/public/images/about-me/about-me-cover.webp';
 import { PAGE_CONTENT } from "@/app/[locale]/(marketing)/about_me/pageContent";
 import { MENU_ITEMS_CONFIG } from "@/app/[locale]/(marketing)/about_me/pageContent";
 
@@ -17,17 +17,17 @@ export default function AboutMePage({ params: { locale } }: { params: { locale: 
     return (
         <Container>
             <div className="w-full">
-                <Image src={aboutMeCover} alt="about_me_cover" className="w-full object-cover max-md:object-[70%] max-md:h-[615px]" priority placeholder="blur" />
+                <Image src={aboutMeCover} alt="about_me_cover" className="w-full object-cover max-md:object-[67%] max-md:h-[615px]" priority placeholder="blur" />
             </div>
-            <div className='px-4 lg:px-10'>
-                <div className="flex justify-between py-6 sm:py-7 md:py-8 lg:py-9 xl:py-10 font-bold text-[36px] leading-[36px] md:leading-[39px] tracking-[.15em]">
+            <div className='px-4 md:px-10'>
+                <div className="flex justify-between py-5 md:py-10 font-bold text-[36px] leading-[36px] md:leading-[39px] tracking-[.15em]">
                     <span className="text-primaryBlue font-roboto-serif">{getTextBySectionName('title')}</span>
                 </div>
-                <div className="text-base lg:text-[24px] lg:leading-[30px] tracking-[.15em] text-black font-helvetica">
+                <div className="text-base md:text-[24px] md:leading-[30px] tracking-[.05em] text-black font-helvetica">
                     <span className={locale === 'ser' ? 'block' : 'inline'}><span className="font-bold">{getTextBySectionName('name')}</span>{getTextBySectionName('born')}</span>
                     <span>{getTextBySectionName('graduated')}</span><span className="font-bold text-primaryBlue">{getTextBySectionName('faculty')}</span>{getTextBySectionName('graduated_at')}<span className="font-bold text-primaryBlue">{getTextBySectionName('department')}</span>
 
-                    <span className='block my-6 sm:my-7 md:my-8 lg:my-9 xl:my-10'>
+                    <span className='block my-4 md:my-6'>
                         <span className="font-bold">{getTextBySectionName('ma_degree')}</span>{getTextBySectionName('ma_degree_details')}
                     </span>
 
@@ -36,7 +36,7 @@ export default function AboutMePage({ params: { locale } }: { params: { locale: 
                     <span className="font-bold text-primaryBlue block">{getTextBySectionName('ulus_details')}</span>
                 </div>
             </div>
-            <div className="flex flex-col max-md:gap-2.5 max-md:mx-4 md:flex-row md:border-y md:border-l border-borderSecondary text-overlayBlue h-fit md:h-[150px] my-10 sm:my-12 md:my-14 lg:my-16 xl:my-20">
+            <div className="flex flex-col max-md:gap-2.5 max-md:mx-4 md:flex-row md:border-y md:border-l border-borderSecondary text-overlayBlue h-fit md:h-[150px] my-5 md:my-7 overflow-hidden">
                 {Object.values(MENU_ITEMS_CONFIG).map((item) => (
                     <Link key={item.ID} href={`/${locale}/${item.LINK}`} replace className='border-b md:border-r md:border-b-0 flex-1 flex justify-center items-center h-[84px] md:h-[150px]'>
                         <Image className="w-auto md:w-[90%] object-cover h-[84px] md:h-auto" src={item.BUTTON[locale.toUpperCase() as keyof ITranslations]} alt={item.ALT} placeholder='blur' priority />
