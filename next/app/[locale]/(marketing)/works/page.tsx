29a038218a92aceb98828dc7d09aee52e8928158
Ivan Balic <balicivan2@gmail.com>
2025-04-26 16:59:52 +0200

    Update my works page

--- next/app/[locale]/(marketing)/works/page.tsx
+++ next/app/[locale]/(marketing)/works/page.tsx
@@ -15,11 +15,11 @@ const getTitle = ( locale: string) => {
 export default function MyWorksPage({ params: { locale } }: { params: { locale: string } }) {
     return (
         <div className='pt-[75px] md:pt-[100px] lg:pt-[125px] bg-overlayBlue h-screen'>
-            <Container className="px-[30px] md:px-[65px] lg:px-[100px] xl:px-[135px]">
-                <div className="my-10 font-bold text-[30px] leading-[33px] md:text-[33px] md:leading-[36px] lg:text-[36px] lg:leading-[39px] tracking-[.15em]">
+            <Container className="px-[16px] md:px-[135px]">
+                <div className="my-10 font-bold text-[36px] leading-[39px] tracking-[.15em] font-roboto-serif">
                     <span>{getTitle(locale)}</span>
                 </div>
-                <div className="flex flex-col gap-2 mb-20 text-[20px] leading-[27px] md:text-[22px] md:leading-[29px] lg:text-[24px] lg:leading-[31px] font-helvetica tracking-[.15em]">
+                <div className="flex flex-col gap-6 text-[16px] md:text-[24px] leading-[16px] md:leading-[24px] font-mono tracking-[.15em]">
                     {PAGE_CONTENT.CONTENT.map((item) => (
                         <Link
                             key={item.ID}
